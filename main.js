d8f9bfd2387c67b44642700f4a4178455b11cc72
Fabio Macedo <fabiomacedomendes@gmail.com>
2019-05-23 15:33:45 -0300

    Sample

--- main.js
+++ main.js
@@ -23,10 +23,20 @@ function randint(a, b) {
 /**
  * Retorna n exemplos a partir da lista sem reposição.
  */
-function sample(lst, n){
-    return lst;
-}
+function sample(lst, n) {
+    let res = [];
+        python= [];
 
+    while (res.length < n) {
+        let l = randint(0, lst.length - 1);
+        if (!python.includes(l)) {
+            res.push(lst[l]);
+            python.push(l);
+        }
+    }
+
+    return res;
+} 
 
 
 
